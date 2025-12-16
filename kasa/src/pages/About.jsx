79080f8422e1f857102018ff95a983f9722c7164
Anastasia said <anastasiasaidfara@gmail.com>
2025-12-16 02:28:58 +0100

    integration rotation icon

--- kasa/src/pages/About.jsx
+++ kasa/src/pages/About.jsx
@@ -9,7 +9,8 @@ function About() {
         <details className="collapse-section">
           <summary className="collapse-header">
             <h3>Fiabilité</h3>
-            <span className="collapse-icon">▼</span>
+            <i className="fa-solid fa-angle-down"></i>
+            
           </summary>
           <div className="collapse-content">
             <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</p>
@@ -19,7 +20,7 @@ function About() {
         <details className="collapse-section">
           <summary className="collapse-header">
             <h3>Respect</h3>
-            <span className="collapse-icon">▼</span>
+            <i className="fa-solid fa-angle-down"></i>
           </summary>
           <div className="collapse-content">
             <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
@@ -29,7 +30,8 @@ function About() {
         <details className="collapse-section">
           <summary className="collapse-header">
             <h3>Service</h3>
-            <span className="collapse-icon">▼</span>
+            <i className="fa-solid fa-angle-down"></i>
+
           </summary>
           <div className="collapse-content">
             <p>La qualité du service est au coeur de notre engagement envers nos clients. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.</p>
@@ -39,7 +41,7 @@ function About() {
         <details className="collapse-section">
           <summary className="collapse-header">
             <h3>Sécurité</h3>
-            <span className="collapse-icon">▼</span>
+            <i className="fa-solid fa-angle-down"></i>
           </summary>
           <div className="collapse-content">
             <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
