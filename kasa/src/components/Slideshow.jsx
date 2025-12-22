efa62d823e5f41aee4dcc500d3df7e081b535e6f
Anastasia said <anastasiasaidfara@gmail.com>
2025-12-22 06:36:39 +0100

    creation slideshow pour le carroussel

---
+++ kasa/src/components/Slideshow.jsx
@@ -0,0 +1,17 @@
+ const logement = {
+                id: id,
+                title: "Cozy loft on the Canal Saint-Martin",
+                location: "Paris, Île-de-France",
+                pictures: [
+                    "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-1-1.jpg",
+                    "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-1-2.jpg"
+                ],
+                description: "Appartement de 55 m² avec vue sur le canal Saint-Martin...",
+                host: {
+                    name: "Alexandre Dumas",
+                    picture: "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/profile-picture-1.jpg"
+                },
+                rating: "5",
+                tags: ["Cozy", "Canal", "Paris 10"],
+                equipments: ["Climatisation", "Wi-Fi", "Cuisine équipée"]
+            };
